d6e65c537bdce819c77e04958ee00a357b46f35d
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-08-24 04:29:13 -0700

    Add reverseString test suite and implement function

--- misc.js
+++ misc.js
@@ -3,13 +3,16 @@ export function capitalize(str) {
   if (str === ``) {
     return ``;
   }
-  if (typeof str === 'undefined') {
+  if (typeof str === "undefined") {
     throw new Error("No input given to capitalize function");
   }
   return `${str[0].toUpperCase()}${str.slice(1)}`;
 }
 
 // reverseString
+export function reverseString(str) {
+  return [...str].reverse().join("");
+}
 
 // calculator
 
--- misc.test.js
+++ misc.test.js
@@ -1,22 +1,40 @@
 import { capitalize } from "./misc.js";
+import { reverseString } from "./misc.js";
 
 // capitalize
-test('simple base case', () => {
-  expect(capitalize('hello!')).toBe('Hello!');
+test("simple base case", () => {
+  expect(capitalize("hello!")).toBe("Hello!");
 });
 
-test('already capitalized', () => {
-  expect(capitalize('Testing')).toBe('Testing');
+test("already capitalized", () => {
+  expect(capitalize("Testing")).toBe("Testing");
 });
 
-test('empty (but present) input', () => {
-  expect(capitalize('')).toBe('');
+test("empty (but present) input", () => {
+  expect(capitalize("")).toBe("");
 });
 
-test('no input', () => {
+test("no input", () => {
   expect(() => capitalize()).toThrow(Error);
 });
 
-test('integer input', () => {
+test("integer input", () => {
   expect(() => capitalize(4)).toThrow(Error);
+});
+
+// reverse string
+test("simple base case", () => {
+  expect(reverseString("hello!")).toBe("!olleh");
+});
+
+test("empty (but present) input", () => {
+  expect(reverseString("")).toBe("");
+});
+
+test("no input", () => {
+  expect(() => reverseString()).toThrow(Error);
+});
+
+test("non-string input", () => {
+  expect(() => reverseString(4)).toThrow(Error);
 });
